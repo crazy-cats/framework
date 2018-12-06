f5830c34fdbcef26b5dcbb4e7862795cd78b8cdd
Bruce <bruce.z@infinitytesting.com.au>
2018-12-06 10:10:18 +0800

    ^ build CrazyCat framework

---
+++ src/Utility/StaticVariable.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Copyright © 2018 CrazyCat, Inc. All rights reserved.
+ * See COPYRIGHT.txt for license details.
+ */
+
+namespace CrazyCat\Framework\Utility;
+
+/**
+ * @category CrazyCat
+ * @package CrazyCat\Framework
+ * @author Bruce Z <152416319@qq.com>
+ * @link http://crazy-cat.co
+ */
+class StaticVariable {
+
+    const NO_SELECTION = '_NO_SELECTION_';
+
+}
