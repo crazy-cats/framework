7b2af9f6d92513c0dee9122701e467cb9b908ff7
zengliwei <zengliwei@163.com>
2020-03-15 20:06:01 +0800

    ^ update copyright

--- src/App.php
+++ src/App.php
@@ -1,34 +1,26 @@
 <?php
 
 /*
- * Copyright © 2018 CrazyCat, Inc. All rights reserved.
+ * Copyright © 2020 CrazyCat, Inc. All rights reserved.
  * See COPYRIGHT.txt for license details.
  */
 
 namespace CrazyCat\Framework;
 
-use CrazyCat\Framework\App\Area;
-use CrazyCat\Framework\App\Cache\Factory as CacheFactory;
-use CrazyCat\Framework\App\Config;
-use CrazyCat\Framework\App\Db\Manager as DbManager;
-use CrazyCat\Framework\App\Handler\ErrorHandler;
-use CrazyCat\Framework\App\Handler\ExceptionHandler;
-use CrazyCat\Framework\App\Io\Factory as IoFactory;
 use CrazyCat\Framework\App\Io\Http\Response\ContentType;
 use CrazyCat\Framework\App\Module\Manager as ModuleManager;
 use CrazyCat\Framework\App\ObjectManager;
 use CrazyCat\Framework\App\Setup\Component as ComponentSetup;
 use CrazyCat\Framework\App\Theme\Manager as ThemeManager;
-use CrazyCat\Framework\App\Translator;
 
 /**
  * @category CrazyCat
- * @package CrazyCat\Framework
- * @author Bruce Z <152416319@qq.com>
- * @link http://crazy-cat.co
+ * @package  CrazyCat\Framework
+ * @author   Liwei Zeng <zengliwei@163.com>
+ * @link     http://crazy-cat.cn
  */
-class App {
-
+class App
+{
     /**
      * @var \CrazyCat\Framework\App\Area
      */
@@ -60,6 +52,11 @@ class App {
     private $errorHandler;
 
     /**
+     * @var App\Handler\ExceptionHandler
+     */
+    private $exceptionHandler;
+
+    /**
      * @var \CrazyCat\Framework\App\Io\Factory
      */
     private $ioFactory;
@@ -86,15 +83,27 @@ class App {
 
     /**
      * Get app singleton
+     *
      * @return \CrazyCat\Framework\App
      */
-    static public function getInstance()
+    public static function getInstance()
     {
-        return App\ObjectManager::getInstance()->get( self::class );
+        return App\ObjectManager::getInstance()->get(self::class);
     }
 
-    public function __construct( CacheFactory $cacheFactory, ExceptionHandler $exceptionHandler, ErrorHandler $errorHandler, DbManager $dbManager, Area $area, IoFactory $ioFactory, Translator $translator, ModuleManager $moduleManager, ComponentSetup $componentSetup, Config $config, ObjectManager $objectManager )
-    {
+    public function __construct(
+        \CrazyCat\Framework\App\Cache\Factory $cacheFactory,
+        \CrazyCat\Framework\App\Handler\ExceptionHandler $exceptionHandler,
+        \CrazyCat\Framework\App\Handler\ErrorHandler $errorHandler,
+        \CrazyCat\Framework\App\Db\Manager $dbManager,
+        \CrazyCat\Framework\App\Area $area,
+        \CrazyCat\Framework\App\Io\Factory $ioFactory,
+        \CrazyCat\Framework\App\Translator $translator,
+        \CrazyCat\Framework\App\Module\Manager $moduleManager,
+        \CrazyCat\Framework\App\Setup\Component $componentSetup,
+        \CrazyCat\Framework\App\Config $config,
+        \CrazyCat\Framework\App\ObjectManager $objectManager
+    ) {
         $this->area = $area;
         $this->cacheFactory = $cacheFactory;
         $this->componentSetup = $componentSetup;
@@ -119,106 +128,116 @@ class App {
     /**
      * @param \Composer\Autoload\ClassLoader $composerLoader
      * @return array
+     * @throws \Exception
      */
-    private function initComponents( $composerLoader )
+    private function initComponents($composerLoader)
     {
-        set_error_handler( [ $this->errorHandler, 'process' ] );
-        set_exception_handler( [ $this->exceptionHandler, 'process' ] );
+        set_error_handler([$this->errorHandler, 'process']);
+        set_exception_handler([$this->exceptionHandler, 'process']);
 
-        profile_start( 'Collect components' );
-        $components = $this->componentSetup->init( $composerLoader );
-        profile_end( 'Collect components' );
+        profile_start('Collect components');
+        $components = $this->componentSetup->init($composerLoader);
+        profile_end('Collect components');
 
-        profile_start( 'Initializing modules' );
-        $this->moduleManager->init( $components[ComponentSetup::TYPE_MODULE] );
-        profile_end( 'Initializing modules' );
+        profile_start('Initializing modules');
+        $this->moduleManager->init($components[ComponentSetup::TYPE_MODULE]);
+        profile_end('Initializing modules');
 
         return $components;
     }
 
     /**
      * @param \Composer\Autoload\ClassLoader $composerLoader
+     * @throws \Exception
      */
-    public function run( $composerLoader, $areaCode = null )
+    public function run($composerLoader, $areaCode = null)
     {
-        profile_start( 'Run APP' );
+        profile_start('Run APP');
 
         /**
          * Use UTC time as system time, for calculation and storage
          */
-        ini_set( 'date.timezone', 'UTC' );
+        ini_set('date.timezone', 'UTC');
 
-        profile_start( 'Initializing components' );
-        $components = $this->initComponents( $composerLoader );
-        profile_end( 'Initializing components' );
+        profile_start('Initializing components');
+        $components = $this->initComponents($composerLoader);
+        profile_end('Initializing components');
 
         /**
          * Dependency Injections
          */
-        profile_start( 'Initializing dependency injections' );
-        $cacheDependencyInjections = $this->cacheFactory->create( ObjectManager::CACHE_DI_NAME );
-        if ( empty( $dependencyInjections = $cacheDependencyInjections->getData() ) ) {
+        profile_start('Initializing dependency injections');
+        $cacheDependencyInjections = $this->cacheFactory->create(ObjectManager::CACHE_DI_NAME);
+        if (empty($dependencyInjections = $cacheDependencyInjections->getData())) {
             $dependencyInjections = $this->moduleManager->collectDependencyInjections();
-            $cacheDependencyInjections->setData( $dependencyInjections )->save();
+            $cacheDependencyInjections->setData($dependencyInjections)->save();
         }
-        $this->objectManager->collectPreferences( $dependencyInjections );
-        profile_end( 'Initializing dependency injections' );
+        $this->objectManager->collectPreferences($dependencyInjections);
+        profile_end('Initializing dependency injections');
 
         /**
          * Translations will be collected on the first usage of `translate` method,
          *     so no need to worry about the area code here.
          */
-        profile_start( 'Initializing translator' );
-        $this->translator->init( $components[ComponentSetup::TYPE_LANG] );
-        profile_end( 'Initializing translator' );
+        profile_start('Initializing translator');
+        $this->translator->init($components[ComponentSetup::TYPE_LANG]);
+        profile_end('Initializing translator');
 
         /**
          * The IO factory creates a suitable request object for runtime environment,
          *     area code is specified in `process` method of the object.
          */
-        profile_start( 'Process request' );
-        $this->request = $this->ioFactory->create( $areaCode );
+        profile_start('Process request');
+        $this->request = $this->ioFactory->create($areaCode);
         $this->request->process();
-        profile_end( 'Process request' );
+        profile_end('Process request');
 
-        if ( $this->request->getModuleName() ) {
-            $this->moduleManager->getModule( $this->request->getModuleName() )
-                    ->launch( $this->area->getCode(), $this->request->getControllerName(), $this->request->getActionName() );
+        if ($this->request->getModuleName()) {
+            $this->moduleManager->getModule($this->request->getModuleName())
+                ->launch($this->area->getCode(), $this->request->getControllerName(), $this->request->getActionName());
         }
 
-        profile_end( 'Run APP' );
+        profile_end('Run APP');
     }
 
     /**
      * @param string $composerLoader
      * @return mixed
+     * @throws \Exception
      */
-    public function getStatic( $composerLoader )
+    public function getStatic($composerLoader)
     {
-        $this->initComponents( $composerLoader );
+        $this->initComponents($composerLoader);
 
-        $path = filter_input( INPUT_GET, 'path' );
-        $pathArr = explode( '/', $path );
+        $path = filter_input(INPUT_GET, 'path');
+        $pathArr = explode('/', $path);
 
         /* @var $theme \CrazyCat\Framework\App\Theme */
-        list( $areaCode, $themeName ) = $pathArr;
-        $theme = $this->objectManager->get( ThemeManager::class )->init()
-                ->getTheme( $areaCode, $themeName );
+        list($areaCode, $themeName) = $pathArr;
+        $theme = $this->objectManager->get(ThemeManager::class)->init()
+            ->getTheme($areaCode, $themeName);
 
         /* @var $module \CrazyCat\Framework\App\Module */
-        if ( isset( $pathArr[4] ) && ( $module = $this->objectManager->get( ModuleManager::class )
-                ->getModule( $pathArr[2] . '\\' . $pathArr[3] ) ) ) {
-            $staticPath = $module['config']['namespace'] . '::' . substr( $path, strlen( $areaCode ) + strlen( $themeName ) + strlen( $module['config']['namespace'] ) + 3 );
-        }
-        else {
-            $staticPath = substr( $path, strlen( $areaCode ) + strlen( $themeName ) + 2 );
+        if (isset($pathArr[4]) && ($module = $this->objectManager->get(ModuleManager::class)
+                ->getModule($pathArr[2] . '\\' . $pathArr[3]))) {
+            $staticPath = $module['config']['namespace'] . '::' . substr(
+                $path,
+                strlen($areaCode) + strlen(
+                    $themeName
+                ) + strlen($module['config']['namespace']) + 3
+            );
+        } else {
+            $staticPath = substr($path, strlen($areaCode) + strlen($themeName) + 2);
         }
 
-        if ( ( $filePath = $theme->getStaticPath( $staticPath ) ) ) {
-            header( 'Content-Type: ' . $this->objectManager->get( ContentType::class )->getByExt( pathinfo( $filePath )['extension'] ) );
-            $theme->generateStaticFile( $areaCode, $themeName, $staticPath );
-            readfile( $filePath );
+        if (($filePath = $theme->getStaticPath($staticPath))) {
+            header(
+                'Content-Type: ' . $this->objectManager->get(ContentType::class)->getByExt(
+                    pathinfo($filePath)['extension']
+                )
+            );
+            $theme->generateStaticFile($areaCode, $themeName, $staticPath);
+            readfile($filePath);
         }
     }
-
 }
